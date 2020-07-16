8a68338178daa52f0682f50b188cd2ec2ea30dc2
danvb112 <danielbonasser@gmail.com>
2020-07-16 12:53:42 -0300

    Termino do CRUD

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const { json } = require('express');
 
 //iniciando o app
 const app = express();
+app.use(express.json());
 
 //conectando com o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
