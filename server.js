d672edcb88b873ce40ee9d6c31886010551d75f5
danvb112 <danielbonasser@gmail.com>
2020-07-17 11:10:32 -0300

    Término do curos

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const requireDir = require('require-dir');
 const { json } = require('express');
 
 //iniciando o app
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 //conectando com o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 requireDir("./src/models")
